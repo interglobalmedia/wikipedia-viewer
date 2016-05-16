6b854b7e6329fb9bc9965e13217b779fadd7bfb3
interglobalmedia <interglobalmedia@gmail.com>
2016-05-16 09:23:27 -0400

    Make Master app.js duplicate of gh-pages app.js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,28 @@
 // Don't need an API key.
 // Making a normal json request to wikipedia would result in a cross origin or cross site error
 // because wikipedia servers forbid cross origin requests.
-// Wikipedia AJAX request goes here
-
 function wikiViewer() {
-  var theTitle = '';
-  var theContent = '';
-  createView();
-  $('input[type="submit"]').on('click', function() {
-    createView();
-  });
-
-  function createView() {
-    var wikiURL;
-    var wikiURLStart = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='
-    var articleStr;
-    var wikiURLEnd = '&format=json&callback=wikiCallback';
-
-    $.ajax({
-        url: wikiURL,
-        type: 'GET',
-        data: {},
-        dataType: 'jsonp',
-        success: function(data) {
-          theTitle = data.title;
-          theContent = data.content;
-          $('#title').html() = theTitle;
-          $('#content').html() = theContent;
-
-          for (var i = 0; i < articleList.length; i++) {
-            articleStr = articleList[i];
-            wikiURL = "http://en.wikipedia.org/wiki/" + articleStr + wikiURLEnd;
-            $wikiElem.append('<li><a href="' + url + '">' + articleStr + wikiURLEnd + '</a></li>');
-          }
-        }
+  $('#searchterm').keyup(function(e) {
+    // query variable
+    var q = $("#searchterm").val();
+    $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+    {
+      srsearch: q,
+      action: "query",
+      list: "search",
+      format: "json"
+    },
+    function(data) {
+      $(".section").empty();
+      $(".section").append("Results for <b>" + q + "</b>");
+      $.each(data.query.search, function(i, item) {
+        $(".section").append("<div id='results'><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a>: " + item.snippet + "</div>");
+      });
     });
-  }
+  });
 }
 wikiViewer();
-/* :)
-
-What needs to be done in this program:
+/* /* What needs to be done in this program:
 
   1. User inputs a search query and submits.
     - We need to take the text-area's input.
@@ -50,8 +32,5 @@ What needs to be done in this program:
   3. Data returned from Wikipedia is structured on the page.
     - Use that JSON to create elements.
   4. User can click an entry to visit the full article.
-    - Each element has a <a href=''> wrapper. Ok. HTML? Shall we?
-    
-    I thought that I had a link to Google Fonts in my HTML. Did you take it to put into the CSS?
-Thank you!
+    - Each element has a <a href=''> wrapper.
 */
