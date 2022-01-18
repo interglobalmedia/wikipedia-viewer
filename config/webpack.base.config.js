935751fd578c6cbbb351532737b3e9b3950dadd6
Maria Campbell <interglobalmedia@gmail.com>
2022-01-17 21:43:31 -0500

    Add configs

---
+++ config/webpack.base.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const webpack = require('webpack')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { merge } = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = env => {
+    const {
+        PLATFORM,
+        VERSION
+    } = env 
+
+    return merge([{
+        mode: env.PLATFORM ? 'development' : 'production',
+        entry:  ['@babel/polyfill', './src/index.js'],
+        output: {
+            filename: PLATFORM === 'production' ? 'scripts/[name].[contenthash].chunk.js' : 'scripts/[name].chunk.js',
+            chunkFilename: PLATFORM === 'production' ?
+                'scripts/[name].[contenthash].chunk.js' : 'scripts/[name].chunk.js',
+            path: path.resolve(__dirname, '../dist')
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
+            }
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(scss|sass|css)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                        {
+                            loader: 'css-loader',
+                        },
+                        'sass-loader'
+                        ]
+                },
+                {
+                    test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[hash:8].[ext]'
+                        }
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            new ESLintPlugin({}),
+            new CleanWebpackPlugin({}),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                styles: 'src/styles.css',
+                inject: true
+            }),
+            new MiniCssExtractPlugin({
+                filename: PLATFORM === 'production' ?
+                    'styles/[name].[hash].css' : '[name].css'
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'src/static', to: '../dist' }
+                ]
+            }),
+            new webpack.DefinePlugin({
+                'process.env.VERSION': JSON.stringify(env.VERSION),
+                'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+
+            })
+        ]
+    }])
+    
+}
---
+++ config/webpack.prod.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin2');
+
+const baseConfig = require('./webpack.base.config');
+
+const prodConfiguration = env => {
+    return merge([
+        {
+            optimization: {
+                splitChunks: {
+                    chunks: 'all',
+                    maxInitialRequests: Infinity,
+                    minSize: 0,
+                    cacheGroups: {
+                        vendor: {
+                            test: / [\\/]node_modules[\\/]/,
+                            name(module) {
+                                // get the name. E.g. node_modules/packageName/not/this/part.js
+                                // or node_modules/packageName
+                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                                // npm package names are URL-safe, but some servers don't like @ symbols
+                                return `npm.${packageName.replace('@', '')}`;
+                            }
+                        },
+                        styles: {
+                            name: 'main',
+                            test: /\.css$/,
+                            chunks: 'all',
+                            enforce: true
+                        }
+                    }
+                },
+                runtimeChunk: 'single',
+                minimizer: [
+                    new UglifyJSPlugin(),
+                    new OptimizeCssAssetsPlugin()
+                ],
+            },
+            plugins: [
+                new WebpackManifestPlugin({
+                    fileName: ('asset-manifest.json')
+                }),
+                new webpack.ids.HashedModuleIdsPlugin(),
+                new Visualizer({ filename: './statistics.html'})
+            ]
+        }
+    ])
+}
+
+module.exports = env => {
+    return merge(baseConfig(env), prodConfiguration(env));
+}
