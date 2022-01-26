7ba979cfc1440fced0991cac6765c810f0306818
Maria Campbell <interglobalmedia@gmail.com>
2022-01-25 21:52:31 -0500

    Add linter fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import './styles/styles.scss';
-import wikiViewerInit from './modules/wikiViewerInit';
-import scrollStep from './modules/scrollStep';
+import './styles/styles.scss'
+import wikiViewerInit from './modules/wikiViewerInit'
+import scrollStep from './modules/scrollStep'
 
-window.onload = wikiViewerInit();
+window.onload = wikiViewerInit()
 
-const clearResults = document.querySelector('.js-search-input');
+const clearResults = document.querySelector('.js-search-input')
 
 clearResults.addEventListener('keydown', function (e) {
-    const chord = e.keyCode || e.which;
-    if (chord === 8) {
-        e.currentTarget.value = '';
-        document.location.reload();
-    }
+	const chord = e.keyCode || e.which
+	if (chord === 8) {
+		e.currentTarget.value = ''
+		document.location.reload()
+	}
 })
 
-const scrollButton = document.querySelector('.scroll');
-scrollButton.addEventListener('click', scrollStep);
+const scrollButton = document.querySelector('.scroll')
+scrollButton.addEventListener('click', scrollStep)
